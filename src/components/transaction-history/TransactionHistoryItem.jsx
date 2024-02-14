ece46f2625b2598878941c6993d46d76f394289b
yura6669 <yurii.shyian.01@gmail.com>
2024-02-14 23:38:59 +0200

    Fix format

--- src/components/transaction-history/TransactionHistoryItem.jsx
+++ src/components/transaction-history/TransactionHistoryItem.jsx
@@ -1,11 +1,41 @@
 import css from './css/TransactionHistoryItem.module.css';
 
-export const TransactionHistoryItem = ({ id, type, amount, currency, isEven }) => { 
-    return (
-        <tr key={id}>
-            <td className={isEven ? css.transaction__history__item__td__even : css.transaction__history__item__td}>{ type }</td>
-            <td className={isEven ? css.transaction__history__item__td__even : css.transaction__history__item__td}>{ amount }</td>
-            <td className={isEven ? css.transaction__history__item__td__even : css.transaction__history__item__td}>{ currency }</td>
+export const TransactionHistoryItem = ({
+  id,
+  type,
+  amount,
+  currency,
+  isEven,
+}) => {
+  return (
+    <tr key={id}>
+      <td
+        className={
+          isEven
+            ? css.transaction__history__item__td__even
+            : css.transaction__history__item__td
+        }
+      >
+        {type}
+      </td>
+      <td
+        className={
+          isEven
+            ? css.transaction__history__item__td__even
+            : css.transaction__history__item__td
+        }
+      >
+        {amount}
+      </td>
+      <td
+        className={
+          isEven
+            ? css.transaction__history__item__td__even
+            : css.transaction__history__item__td
+        }
+      >
+        {currency}
+      </td>
     </tr>
-    );
+  );
 };
